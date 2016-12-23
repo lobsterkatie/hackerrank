154c0991753ecf1cf9d016fc1658877d52a59018
Katie Byers <lobsterkatie@gmail.com>
2016-12-23 14:23:06 -0800

    fixed formatting

--- algorithms/warmup/solve_me_first.py
+++ algorithms/warmup/solve_me_first.py
@@ -11,13 +11,13 @@
 
 # Input Format
 
-# Code that reads input from stdin is provided for you in the editor. There are
-# lines of input, and each line contains a single integer.
+#     Code that reads input from stdin is provided for you in the editor. There
+#     are lines of input, and each line contains a single integer.
 
 # Output Format
 
-# Code that prints the sum calculated and returned by solveMeFirst is provided
-# for you in the editor.
+#     Code that prints the sum calculated and returned by solveMeFirst is
+#     provided for you in the editor.
 
 
 def solveMeFirst(a, b):
